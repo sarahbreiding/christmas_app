42363b05e3f39d005c887f391c5a12bb8a3aa8aa
Sarah Breiding <breiding.sarah@gmail.com>
2015-10-08 19:16:06 -0400

    recreate gifts table

--- db/migrate/20150924230201_create_gifts.rb
+++ db/migrate/20150924230201_create_gifts.rb
@@ -2,7 +2,7 @@ class CreateGifts < ActiveRecord::Migration
   def change
     create_table :gifts do |t|
       t.string :name
-      t.string :status
+      t.boolean :status
       t.string :location
       t.references :person, index: true
 
--- db/migrate/20151008151416_change_data_type_for_gift_status.rb
+++
@@ -1,9 +0,0 @@
-class ChangeDataTypeForGiftStatus < ActiveRecord::Migration
-  def self.up
-    change_column :gifts, :status, :boolean
-  end
-
-  def self.down
-    change_column :gifts, :status, :string
-  end
-end
