1fdc40315524052c2cb41f0df543647d16d78ffc
Sarah Breiding <breiding.sarah@gmail.com>
2016-10-24 08:45:32 -0400

    change archive to checkbox

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -1,5 +1,5 @@
 class Gift < ActiveRecord::Base
   belongs_to :person
-  scope :unarchived, -> { where(archive: false || nil)}
+  scope :unarchived, -> { where(archive: false)}
   scope :archived, -> { where(archive: true )}
 end
